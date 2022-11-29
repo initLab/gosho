0e823473abeb052f4c320988ab96d0ce9064d24c
Vencislav Atanasov <git@6bez10.info>
2022-11-29 02:40:13 +0200

    Use macro instead of toggling the pins directly

--- backends/netcontrol.js
+++ backends/netcontrol.js
@@ -4,36 +4,35 @@ const request = require('superagent');
 
 const NetcontrolBackend = config => {
 	const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-	
+
 	const makeRequest = url => request.get(url)
 		.auth(config.username, config.password);
-	
-	const toggleState = newState => makeRequest(`${config.url}/iochange.cgi?ref=ioreg.js&${config.latchControlPin}=${newState ? '01' : '00'}`)
-		.then(() => sleep(500));
-	
+
+	const toggleState = newState => makeRequest(`${config.url}/params.cgi?ref=re-io&0A=${newState ?
+		config.lockMacroNumber : config.unlockMacroNumber}`)
+		.then(() => sleep(config.operationTimeMs));
+
 	return {
 		getState: () => {
 			return request.get(`${config.url}/ioreg.js`)
 				.auth(config.username, config.password)
 				.then(res => res.body.toString())
 				.then(data => {
-					let ioString = data.match(/var\ IO\=\"([0-9A-Z,]+)\"/);
+					let ioString = data.match(/var IO="([0-9A-Z,]+)"/);
 					let arr = ioString[1].split(',');
 					console.log(arr);
 					let sensors = {
-						doorState: parseInt(arr[config.doorStatePin], 16) < 512,
-						lockButton: parseInt(arr[config.lockButtonPin], 16) < 512,
-						unlockButton: parseInt(arr[config.unlockButtonPin], 16) < 512,
-						locked: parseInt(arr[config.lockedSensorPin], 16) < 512,
-						unlocked: parseInt(arr[config.unlockedSensorPin], 16) < 512
+						doorLocked: parseInt(arr[config.doorLockedPin], 16) < 512,
+						doorUnlocked: parseInt(arr[config.doorUnlockedPin], 16) < 512,
+						doorClosed: parseInt(arr[config.doorClosedPin], 16) < 512,
 					};
 					console.log(sensors)
 					let state = {
-						latch: sensors.locked && !sensors.unlocked ? 'locked' : (
-							!sensors.locked && sensors.unlocked ? 'unlocked' :
+						latch: sensors.doorLocked && !sensors.doorUnlocked ? 'locked' : (
+							!sensors.doorLocked && sensors.doorUnlocked ? 'unlocked' :
 								'unknown' // failsafe
 						),
-						door: sensors.doorState ? 'closed' : 'open',
+						door: sensors.doorClosed ? 'closed' : 'open',
 					}
 					console.log(state);
 					return state;
